5de0455cdd595f83ae03b2145245b3c933c6a507
Bradley Fargo <blewfargs@gmail.com>
2019-05-02 13:20:52 -0400

    did wiki tut, set goals for next sesh

--- gwikay.go
+++ gwikay.go
@@ -1,9 +1,135 @@
 package main
 
+// props:  https://golang.org/doc/articles/wiki/
+
+// additional tasks:
+
+/*
+Here are some simple tasks you might want to tackle on your own:
+
+- Store templates in tmpl/ and page data in data/.
+- Add a handler to make the web root redirect to /view/FrontPage.
+- Spruce up the page templates by making them valid HTML and adding some CSS rules.
+- Implement inter-page linking by converting instances of [PageName] to
+  <a href="/view/PageName">PageName</a>. (hint: you could use regexp.ReplaceAllFunc to do this)
+
+- implement an audio cue for hitting save (using beep)
+*/
+
 import (
-"fmt"
+  "io/ioutil"
+  "net/http"
+  "log"
+  "regexp"
+  "errors"
+
+  "html/template"
 )
 
-func main() {
-  fmt.Println("Oi")
+/// regexp (shield from rando files written)
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-z0-9]+)$")
+/// templating
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+  err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
+
+/////
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+  m := validPath.FindStringSubmatch(r.URL.Path)
+  if m == nil {
+    http.NotFound(w, r)
+    return "", errors.New("Invalid Page Title Yo")
+  }
+  return m[2], nil // title is second subexpression
+}
+
+///
+
+
+func main() {
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
+
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+/////
+
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
+    }
+    fn(w, r, m[2]) // title is second subexpression
+    // extracted page title from request and called the provided handler fn
+  }
+}
+
+///
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+  p, err := loadPage(title)
+  if err != nil {
+    http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+    return
+  }
+  renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{Title: title}
+  }
+  renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  err := p.save()
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+/// Page
+
+type Page struct {
+  Title string
+  Body []byte
+}
+
+func (p *Page) save() error {
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+  filename := title + ".txt"
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title: title, Body: body}, nil
+}
+
+/// end Page
+
+// func main() {
+//   p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Test Page.")}
+//   p1.save()
+//   p2, _ := loadPage("TestPage")
+//   fmt.Println(string(p2.Body))
+// }
